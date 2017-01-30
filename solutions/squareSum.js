9414c0b291c2cd97e29b9057cac57cf87070b3b7
Toby Chow <Toby_Chow@hotmail.com>
2017-01-30 17:56:06 -0500

    Add different solutions (#7)
    * add method using es2015 spread operator
    
    * add method using reduce

--- solutions/squareSum.js
+++ solutions/squareSum.js
@@ -1,8 +1,9 @@
-function squareSum(arrayOfNumbers){
-  var result = 0;
-  for (i=0; i<arrayOfNumbers.length; i++) {
-    result += arrayOfNumbers[i] * arrayOfNumbers[i];
-  }
-  return result;
+// different method using reduce
+function squareSum(arr){
+	var sum = arr.reduce((sum,n)=>{
+		return (n*n)+sum;
+	},0);
+	console.log(sum);
 }
-console.log(squareSum([2,3,4,5,6]));
+
+ squareSum([1, 2, 2]);  // 9
